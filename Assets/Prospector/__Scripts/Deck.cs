66a2fc10d8e34c648b47cd19b97778d9a7e33ef6
Acetorrent <phihoangkhai2811@gmail.com>
2023-03-09 20:09:10 -0500

    published

--- Assets/Prospector/__Scripts/Deck.cs
+++ Assets/Prospector/__Scripts/Deck.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(JsonParseDeck))]
+[RequireComponent(typeof(JsonParseDeck))]
+// a
 public class Deck : MonoBehaviour
 {
-
     [Header("Inscribed")]
     public CardSpritesSO cardSprites;
     public GameObject prefabCard;
@@ -15,78 +15,116 @@ public class Deck : MonoBehaviour
     [Header("Dynamic")]
     public Transform deckAnchor;
     public List<Card> cards;
-    private JsonParseDeck jsonDeck;
 
+    private JsonParseDeck jsonDeck;
     static public GameObject SPRITE_PREFAB { get; private set; }
 
 
-    // Start is called before the first frame update
-    /*void Start()
-    {
-        InitDeck();
-        Shuffle(ref cards);
-    }
+    /*
+        void Start()
+        {
+            InitDeck();
+            Shuffle(ref cards);
+        }
     */
+
+
+
+    /// <summary>
+    /// The Prospector class will call InitDeck to set up the deck and build
+    /// all 52 card GameObjects from the jsonDeck and cardSprites information.
+    /// </summary> > 
     public void InitDeck()
     {
+        // Create a static reference to spritePrefab for the Card class to use
         SPRITE_PREFAB = prefabSprite;
-
+        // Call Init method on the CardSpriteSO instance assigned to cardSprites
         cardSprites.Init();
-        jsonDeck = GetComponent<JsonParseDeck>();
 
+        // Get a reference to the JsonParseDeck component
+        jsonDeck = GetComponent<JsonParseDeck>();
+        // b
+
+        // Create an anchor for all the Card GameObjects in the Hierarchy
         if (GameObject.Find("_Deck") == null)
         {
+            // c
             GameObject anchorGO = new GameObject("_Deck");
             deckAnchor = anchorGO.transform;
         }
 
         MakeCards();
-
     }
 
+    /// <summary>
+    /// Create a GameObject for each card in the deck.
+    /// </summary>
     void MakeCards()
     {
         cards = new List<Card>();
         Card c;
 
+        // Generate 13 cards for each suit
         string suits = "CDHS";
         for (int i = 0; i < 4; i++)
         {
             for (int j = 1; j <= 13; j++)
             {
+                // d
                 c = MakeCard(suits[i], j);
+                // e
+                cards.Add(c);
 
-                cards.Add (c);
-
+                // This aligns the cards in nice rows fortesting
                 c.transform.position = new Vector3((j - 7) * 3, (i - 1.5f) * 4, 0);
             }
         }
     }
 
-    Card MakeCard (char suit, int rank)
+    /// <summary>
+    /// Creates a card GameObject based on suit and rank.
+    /// Note that this method assumes it will be passed avalid suit and rank.
+    /// </summary>
+    /// <param name="suit">The suit of the card (e.g., ’C’)</param>
+    /// <param name="rank">The rank from 1 to 13</param>
+    /// <returns></returns>
+    Card MakeCard(char suit, int rank)
     {
-        GameObject go = Instantiate<GameObject>(prefabCard, deckAnchor);
+        GameObject go = Instantiate<GameObject>(prefabCard, deckAnchor); // f
         Card card = go.GetComponent<Card>();
 
         card.Init(suit, rank, startFaceUp);
-
+        // g
         return card;
     }
 
-    static public void Shuffle (ref List<Card> refCards)
+
+    /// <summary>
+    /// Shuffle a List(Card) and return the result to the original list. // b
+    /// </summary>
+    /// <param name="refCards">As a ref, this alters on the original list</param>
+    static public void Shuffle(ref List<Card> refCards)
     {
+        // a
+        // Create a temporary List to hold the new shuffle order
         List<Card> tCards = new List<Card>();
 
-        int ndx;
-    
-        while (refCards.Count > 0 )
+        int ndx; // This will hold the index of the card to be moved
+                 // Repeat as long as there are cards in the original List
+        while (refCards.Count > 0)
         {
+            // Pick the index of a random card
             ndx = Random.Range(0, refCards.Count);
+            // Add that card to the temporary List
             tCards.Add(refCards[ndx]);
+            // And remove that card from the original List
             refCards.RemoveAt(ndx);
         }
-
+        // Replace the original List with the temporary List
         refCards = tCards;
+        // c
     }
-  
+
+
 }
+
--- Assets/Prospector/__Scripts/UITextManager.cs
+++ Assets/Prospector/__Scripts/UITextManager.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
 public class UITextManager : MonoBehaviour
 {
-    private static UITextManager S;
+    private
+static UITextManager S;
 
     [Header("Inscribed")]
     public TMP_Text gameOverText;
@@ -15,16 +15,18 @@ public class UITextManager : MonoBehaviour
     [Header("Dynamic")]
     [SerializeField]
     private bool _resultsUIFieldsVisible = false;
+
     public bool resultsUIFieldsVisible
     {
-        get { return _resultsUIFieldsVisible; }        private set
+        get { return _resultsUIFieldsVisible; }
+        private set
         {
             _resultsUIFieldsVisible = value;
             gameOverText.gameObject.SetActive(value);
-            roundResultText.gameObject.SetActive(value);
+            roundResultText.gameObject.SetActive(value);
         }
     }
-    // Start is called before the first frame update
+
     void Start()
     {
         if (S != null) Debug.LogWarning("Attempt to set Singleton S again!");
@@ -36,42 +38,47 @@ public class UITextManager : MonoBehaviour
 
     void ShowHighScore()
     {
-        string str = $"High Score: {ScoreManager.HIGH_SCORE:#,##0}";
+        // The 0 in "#,##0" on the following line is a zero.
+        string str = $"High Score:{ScoreManager.HIGH_SCORE:#,##0}";
         highScoreText.text = str;
     }
 
-    static public void GAME_OVER_UI (bool won)
+    /// <summary>
+    /// Static wrapper for S.GameOverUI(). Called when the gameis over.
+    /// </summary>
+    /// <param name="won">True if the player won.</param>
+    static public void GAME_OVER_UI(bool won)
     {
         S.GameOverUI(won);
     }
 
-    public void GameOverUI(bool won) 
-    
+    /// <summary>
+    /// Called when the game is over.
+    /// </summary>
+    /// <param name="won">True if the player won.</param>
+    public void GameOverUI(bool won)
     {
         int score = ScoreManager.SCORE;
         string str;
         if (won)
         {
             gameOverText.text = "Round Over";
-            str = "You won this round!\n" + $"Round Score: {ScoreManager.SCORE_THIS_ROUND:#,##0}";
-
+            str = "You won this round!\n" + $"Round Score:{ScoreManager.SCORE_THIS_ROUND:#,##0}";
         }
         else
         {
             gameOverText.text = "Game Over";
             if (ScoreManager.HIGH_SCORE <= score)
             {
-                str = $"You got the high score!\nHigh score: {score: #,##0}";
+                str = $"You got the high score!\nHigh score: {score:#,##0}";
             }
             else
             {
                 str = $"Your final score was:\n{score:#,##0}";
             }
         }
-
         roundResultText.text = str;
         resultsUIFieldsVisible = true;
-        ShowHighScore();
+        ShowHighScore(); // Update the high score shown at the and of each round.
     }
-
 }
